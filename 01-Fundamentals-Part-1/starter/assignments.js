7a2b323a3ec3aaeb835e049612b8481e9dddaab7
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-01-28 13:24:29 +0200

    JS-Fundamentals-Part-1_2.10-assignments

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+let countryName = "Lithuania";
+let continentName = "Europe";
+let population = "2.801 million";
+
+console.log(countryName, continentName, population);
+console.log(countryName);
+console.log(continentName);
+console.log(population);
+console.log(`${countryName}, ${continentName}, ${population}`);
