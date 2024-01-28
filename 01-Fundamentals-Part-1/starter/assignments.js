2df02917e7d888cadb4b781e904426a39d5e6a32
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-01-28 16:43:05 +0200

    JS-Fundamentals-Part-1_2.11-assignments-data-types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,5 @@
+//VALUE AND VARIABLES
+
 let countryName = "Lithuania";
 let continentName = "Europe";
 let population = "2.801 million";
@@ -7,3 +9,11 @@ console.log(countryName);
 console.log(continentName);
 console.log(population);
 console.log(`${countryName}, ${continentName}, ${population}`);
+
+// DATA TYPES
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof countryName);
+console.log(typeof language);
