2b055801eb01d77a157a38b697232ea5285a3bdb
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-01-28 13:08:30 +0200

    JS-Fundamentals-Part-1_2.9-lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,34 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let jonas&matilda = "JM"
+let jonas_matilda = "JM";
+console.log(jonas_matilda);
+
+// let new = 27; Nenaudojami vardai - new, function - rezered keywords in JS !!!
+// let function = 27;
+let _function = 27; // toks galimas variantas
+let $funktion = 27; //toks galimas variantas
+// let name = "Jonas"; tokio geriau nenaudoti - name
+// let firstName = "Jonas"; Gerai sitaip rasyti
+// let 3years = 3; Variable name can not start with a number !
+// let Person = "jonas"; Negalimas vardas iš didžiosios raidės
+let PI = 3.1415; // Name uppercase is reserved for constant that we know will never channge
+
+// GERAS PAVYZDYS - rašyti aiškius variable names
+let myFirstJob = "programmer";
+let muCurrentJob = "Teacher";
+
+// BLOGAS SPAVYZDYS - neaiškūs variable names
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
