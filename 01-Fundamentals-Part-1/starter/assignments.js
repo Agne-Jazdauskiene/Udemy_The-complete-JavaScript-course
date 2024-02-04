a4d13c226e71a133f6fe01c6cfa4c5d4bfe27026
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-02-04 13:26:54 +0200

    JS-Fundamentals-Part-1_2.13

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -4,7 +4,7 @@
 const countryName = "Lithuania";
 // let continentName = "Europe";
 const continentName = "Europe";
-let population = "2.801 million";
+let population = 2.801;
 
 console.log(countryName, continentName, population);
 console.log(countryName);
@@ -26,3 +26,32 @@ language = "Lithuanian";
 
 language = "English";
 console.log(language);
+
+// Basic Operators
+// If your country split in half, and each half would contain half the population, then how many people would live in each half?
+let populationHalf = population / 2;
+console.log(populationHalf);
+
+// Increase the population of your country by 1 and log the result to the console.
+populationHalf++;
+console.log(populationHalf);
+
+// Finland has a population of 6 million. Does your country have more people than Finland?
+// const finlandPopulation = 6;
+console.log(population > 6);
+
+// The average population of a country is 33 million people. Does you country have less people than the average country?
+// const averagePopulation = 33;
+console.log(population < 33);
+
+// Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
+
+let description =
+  countryName +
+  " is in " +
+  continentName +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
